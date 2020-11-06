0f1aead7b3dd2b15a23803e20dfe6265c87edde7
McDAC <47005868+McDAC@users.noreply.github.com>
2020-11-06 22:04:17 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -35,6 +35,12 @@ echo '2.4 создание разделов'
   echo +20G;
 
   echo n;
+  echo;
+  echo;
+  echo;
+  echo +1024M;
+
+  echo n;
   echo p;
   echo;
   echo;
@@ -50,21 +56,23 @@ fdisk -l
 echo '2.4.2 Форматирование дисков'
 mkfs.ext2  /dev/sda1 -L boot
 mkfs.ext4  /dev/sda2 -L root
-mkfs.ext4  /dev/sda3 -L home
+mkswap /dev/sda3 -L swap
+mkfs.ext4  /dev/sda4 -L home
 
 echo '2.4.3 Монтирование дисков'
 mount /dev/sda2 /mnt
 mkdir /mnt/{boot,home}
 mount /dev/sda1 /mnt/boot
-mount /dev/sda3 /mnt/home
+swapon /dev/sda3
+mount /dev/sda4 /mnt/home
 
 echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
 echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 
 echo '3.2 Установка основных пакетов'
-pacstrap /mnt base base-devel
+pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
 
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt sh -c "$(curl -fsSL https://github.com/McDAC/arch2018arch2.sh)"
+arch-chroot /mnt sh -c "$(curl -fsSL git.io/arch2.sh)"
